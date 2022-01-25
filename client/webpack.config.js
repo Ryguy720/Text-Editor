5095fb0a48ca6be043b9dc2f2ef8c2c7f64d666e
unknown <mccullochryan636@gmail.com>
2022-01-25 15:13:23 -0700

    debugging

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Contact Cards'
+        title: 'J.A.T.E.'
       }),
      
       // Injects our custom service worker
@@ -33,9 +33,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
+        name: 'Text Editor',
+        short_name: 'Text Editor',
+        description: 'Easy way to take notes!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: '/',
